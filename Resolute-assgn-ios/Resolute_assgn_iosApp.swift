b90b056dfc07c1ba61a7686bb46d4347bc4c61d4
Pranav <pranav1160ly@gmail.com>
2025-02-27 18:07:19 +0530

    Initial Commit

---
+++ Resolute-assgn-ios/Resolute_assgn_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resolute_assgn_iosApp.swift
+//  Resolute-assgn-ios
+//
+//  Created by Pranav on 27/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Resolute_assgn_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
